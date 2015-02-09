599a43869a2e2eae6280bde62c76678fe7448821
Lauren Speers <speers.lauren@gmail.com>
2015-02-08 23:32:57 -0800

    Fixed some errors in RGB class

--- RGB.cpp
+++ RGB.cpp
@@ -6,49 +6,49 @@ RGB::RGB(float red, float green, float blue) {
 	b = blue;
 }
 
-RGB RBG::rgb_add(RGB color1, RGB color2) const {
+RGB RGB::rgb_add(RGB color1, RGB color2) const {
 	return RGB(color1.get_r()+color2.get_r(), color1.get_g()+color2.get_g(), color1.get_b()+color2.get_b());
 }
 
-RGB RBG::rgb_sub(RGB color1, RGB color2) const {
+RGB RGB::rgb_sub(RGB color1, RGB color2) const {
 	return RGB(color1.get_r()-color2.get_r(), color1.get_g()-color2.get_g(), color1.get_b()-color2.get_b());
 }
 
-RGB RBG::rgb_mult(RGB color1, RGB color2) const {
+RGB RGB::rgb_mult(RGB color1, RGB color2) const {
 	return RGB(color1.get_r()*color2.get_r(), color1.get_g()*color2.get_g(), color1.get_b()*color2.get_b());
 }
 
-RGB scalar_mult(RGB color, float scalar) const {
+RGB RGB::scalar_mult(RGB color, float scalar) const {
 	return RGB(color.get_r()*scalar, color.get_g()*scalar, color.get_b()*scalar);
 }
 
-RGB scalar_mult(RGB color, float scalar) const {
+RGB RGB::scalar_mult(RGB color, float scalar) const {
 	if (0 == scalar) { // Can't divide by 0
 		scalar = 1.0;
 	}
 	return RGB(color.get_r()/scalar, color.get_g()/scalar, color.get_b()/scalar);
 }
 
-void udpate_r(float red){
+void RGB::udpate_r(float red){
 	r = red;
 }
 
-void udpate_g(float green){
+void RGB::udpate_g(float green){
 	g = green;
 }
 
-void udpate_b(float blue){
+void RGB::udpate_b(float blue){
 	b = blue;
 }
 
-float get_r(){
+float RGB::get_r(){
 	return r;
 }
 
-float get_g(){
+float RGB::get_g(){
 	return g;
 }
 
-float get_b(){
+float RGB::get_b(){
 	return b;
 }
