3979f0494e0a475e8962e956d9affc385f96e682
jennypullman <jennypullman@berkeley.edu>
2015-02-09 10:22:39 -0800

    got rid of namespace

--- Vector3.cpp
+++ Vector3.cpp
@@ -1,35 +1,44 @@
 #include <stdexcept>
 #include "Vector3.h"
-	class Vector3 {
-		int x;
-		int y;
-		int z;
-		Vector3(int x, int y, int z){
-			this->x = x;
-			this->y = y;
-			this->z = z;
+class Vector3 {
+	int x;
+	int y;
+	int z;
+	Vector3(int x, int y, int z){
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+	int getX(){
+		return x;
+	}
+	int getY(){
+		return y;
+	}
+	int getZ(){
+		return z;
+	}
+	static Vector3 add(static Vector3 vec1, static Vector3 vec2){
+		return Vector3(vec1.x + vec2.x, vec1.y + vec2.y, vec1.z + vec2.z);
+	}
+	//return vec1 - vec2
+	static Vector3 subtract(Vector3 vec1, Vector3 vec2){
+		return Vector3(vec1.x - vec2.x, vec1.y - vec2.y, vec1.z - vec2.z);
+	}
+	static int dot(Vector3 vec1, Vector3 vec2){
+		return vec1.x*vec2.x + vec1.y*vec2.y + vec1.z*vec2.z;
+	}
+	//return vec1 x vec2
+	static Vector3 cross(Vector3 vec1, Vector3 vec2){
+		return Vector3(vec1.y*vec2.z - vec1.z*vec2.y, vec1.z*vec2.x - vec1.x*vec2.z, vec1.x*vec2.y - vec1.y*vec2.x);
+	}
+	static Vector3 scalarMultiply(Vector3 vec, int i){
+		return Vector3(vec.x*i, vec.y*i, vec.z*i);
+	}
+	static Vector3 scalarDivide(Vector3 vec, int i){
+		if (i != 0) {
+			return Vector3(vec.x / i, vec.y / i, vec.z / i);
 		}
-		static Vector3 add(Vector3 vec1, Vector3 vec2){
-			return Vector3(vec1.x + vec2.x, vec1.y + vec2.y, vec1.z + vec2.z);
-		}
-		//return vec1 - vec2
-		static Vector3 subtract(Vector3 vec1, Vector3 vec2){
-			return Vector3(vec1.x - vec2.x, vec1.y - vec2.y, vec1.z - vec2.z);
-		}
-		static int dot(Vector3 vec1, Vector3 vec2){
-			return vec1.x*vec2.x + vec1.y*vec2.y + vec1.z*vec2.z;
-		}
-		//return vec1 x vec2
-		static Vector3 cross(Vector3 vec1, Vector3 vec2){
-			return Vector3(vec1.y*vec2.z - vec1.z*vec2.y, vec1.z*vec2.x - vec1.x*vec2.z, vec1.x*vec2.y - vec1.y*vec2.x);
-		}
-		static Vector3 scalarMultiply(Vector3 vec, int i){
-			return Vector3(vec.x*i, vec.y*i, vec.z*i);
-		}
-		static Vector3 scalarDivide(Vector3 vec, int i){
-			if (i != 0) {
-				return Vector3(vec.x / i, vec.y / i, vec.z / i);
-			}
-			throw std::overflow_error("Divide by zero error.");
-		}
-	};
+		throw std::overflow_error("Divide by zero error.");
+	}
+};
--- Vector3.h
+++ Vector3.h
@@ -1,15 +1,20 @@
 #include <stdexcept>
 class Vector3{
+public:
+	Vector3(int x, int y, int z);
+	int getX();
+	int getY();
+	int getZ();
+	static Vector3 add(static Vector3 vec1, static Vector3 vec2);
+	//return vec1 - vec2
+	Vector3 subtract(Vector3 vec1, Vector3 vec2);
+	static int dot(Vector3 vec1, Vector3 vec2);
+	//return vec1 x vec2
+	Vector3 cross(Vector3 vec1, Vector3 vec2);
+	Vector3 scalarMultiply(Vector3 vec, int i);
+	Vector3 scalarDivide(Vector3 vec, int i);
+private:
 	int x;
 	int y;
 	int z;
-	Vector3(int x, int y, int z);
-	static Vector3 add(Vector3 vec1, Vector3 vec2);
-	//return vec1 - vec2
-	static Vector3 subtract(Vector3 vec1, Vector3 vec2);
-	static int dot(Vector3 vec1, Vector3 vec2);
-	//return vec1 x vec2
-	static Vector3 cross(Vector3 vec1, Vector3 vec2);
-	static Vector3 scalarMultiply(Vector3 vec, int i);
-	static Vector3 scalarDivide(Vector3 vec, int i);
 };
