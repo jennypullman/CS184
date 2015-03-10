02d5049b3e2b3de6281723de2a15afe617396af4
Lauren Speers <speers.lauren@gmail.com>
2015-03-10 15:05:38 -0700

    updated Image.h

--- A2/Image.h
+++ A2/Image.h
@@ -2,16 +2,15 @@
 
 class Image {
   public:
-    Image(int height, int width);                         // constructor; initialize the height and width
+    Image(int height, int width);                         // constructor; initialize the height and width in pixels
 
-    int get_z(int x, int y) const;                              // get z position for position (x,y) in zCoords array
+    Color get_Color(int x, int y);                            // get rgb object for position (x,y) in rgb array
+    void add_Color(int pixelNum, Color color);                // set element for pixelNum'th pixel to color in rgb array
 
-    Color get_rgb(int x, int y) const;                            // get rgb object for position (x,y) in rgb array
-    void set_rgb(int x, int y, Color color);                // set element for position (x,y) to color in rgb array
+    void printToFile();                                   // output rgb array as an image file TODO determine format and return value
 
   private:
-    float *zCoords;                      			      // zCoords points to dynamically allocated array that gives z coord for each (x,y) pair
-    Color *rgb;                    						  // rgb points to dynamically allocated array that gives rgb object for each (x,y) pair
+    Color *rgb;                    						  // rgb points to dynamically allocated array that gives Color object for each (x,y) pair
     int h;                                                // height
     int w;                                                // width               
 };
