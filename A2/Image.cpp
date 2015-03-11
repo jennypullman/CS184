95874d31721358b23eb95475505a01a60912643d
Lauren Speers <speers.lauren@gmail.com>
2015-03-11 11:27:14 -0700

    Image implemented, but not tested or running because of issues with libpng

--- A2/Image.cpp
+++ A2/Image.cpp
@@ -1,24 +1,115 @@
 #include "Image.h"
 
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h> // TODO decide if this is necessary
+#include "libpng-1.6.16/png.h"
+
 Image::Image(int height, int width) {
 	h = height;
 	w = width;
+	numPixels = h*w;
 
-	zCoords = new float[height*width];
+	// zCoords = new float[height*width];
 	rgb = new Color[height*width];
 
-	// TODO Initialize these arrays
 }
 
-int Image::get_z(int x, int y) const {
-	return *(zCoords + x + w*y); // index (x,y) = y*(#elems/row) + x
+// int Image::get_z(int x, int y) const {
+// 	return *(zCoords + x + w*y); // index (x,y) = y*(#elems/row) + x
+// }
+
+
+Color Image::get_Color(int pixelNum) {		// get rgb object for position (x,y) in rgb array
+	return *(rgb + w*((int) pixelNum/w) + pixelNum%w); 
 }
 
-
-Color Image::get_rgb(int x, int y) const {
-	return *(rgb + x + w*y); // index (x,y) = y*(#elems/row) + x
+void Image::add_Color(int pixelNum, Color color) {		// set element for pixelNum'th pixel to color in rgb array
+	*(rgb + w*((int) pixelNum/w) + pixelNum%w) = color;
 }
 
-void Image::set_rgb(int x, int y, Color color){
-	*(rgb + x + w*y) = color; // index (x,y) = y*(#elems/row) + x
+int Image::printToFile(char* filename){
+	int code = 0;
+	FILE *fp;
+	png_structp png_ptr;
+	png_infop info_ptr;
+	png_bytep row;
+
+	// Open file for writing (binary mode)
+	fp = fopen(filename, "wb");
+	if (fp == NULL) {
+		fprintf(stderr, "Could not open file %s for writing\n", filename);
+		code = 1;
+		goto finalise;
+	}
+
+	// Initialize write structure
+	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if (png_ptr == NULL) {
+		fprintf(stderr, "Could not allocate write struct\n");
+		code = 1;
+		goto finalise;
+	}
+
+	// Initialize info structure
+	info_ptr = png_create_info_struct(png_ptr);
+	if (info_ptr == NULL) {
+		fprintf(stderr, "Could not allocate info struct\n");
+		code = 1;
+		goto finalise;
+	}
+
+	png_init_io(png_ptr, fp);
+
+	// Write header (8 bit colour depth)
+	png_set_IHDR(png_ptr, info_ptr, w, h,
+			8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
+			PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+
+	// Set title
+	png_text title_text;
+	title_text.compression = PNG_TEXT_COMPRESSION_NONE;
+	title_text.key = "Title";
+	title_text.text = filename;
+	png_set_text(png_ptr, info_ptr, &title_text, 1);
+
+	png_write_info(png_ptr, info_ptr);
+
+	// Allocate memory for one row (3 bytes per pixel - RGB)
+	row = (png_bytep) malloc(3 * w * sizeof(png_byte));
+
+	// Write image data
+	int x, y;
+	for (y=0 ; y<h ; y++) {
+		for (x=0 ; x<w ; x++) {
+			row[x*3] = rgb[y*w+x].get_r();
+			row[x*3+1] = rgb[y*w+x].get_g();
+			row[x*3+2] = rgb[y*w+x].get_b();
+		}
+		png_write_row(png_ptr, row);
+	}
+
+	// End write
+	png_write_end(png_ptr, NULL);
+
+	finalise:
+	if (fp != NULL) fclose(fp);
+	if (info_ptr != NULL) png_free_data(png_ptr, info_ptr, PNG_FREE_ALL, -1);
+	if (png_ptr != NULL) png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
+	if (row != NULL) free(row);
+
+	return code;
+}
+
+int Image::getNumPixelsHoriz(){
+	return w;
+}
+	
+int Image::getNumPixelsVert(){
+	return h;
+}
+
+int Image::getNumPixels() {
+	return numPixels;
 }
--- A2/Image.h
+++ A2/Image.h
@@ -4,13 +4,18 @@ class Image {
   public:
     Image(int height, int width);                         // constructor; initialize the height and width in pixels
 
-    Color get_Color(int x, int y);                            // get rgb object for position (x,y) in rgb array
+    Color get_Color(int pixelNum);                            // get rgb object for position (x,y) in rgb array
     void add_Color(int pixelNum, Color color);                // set element for pixelNum'th pixel to color in rgb array
 
-    void printToFile();                                   // output rgb array as an image file TODO determine format and return value
+    int printToFile(char* filename);                                   // output rgb array as an image file TODO determine format and return value
+
+    int getNumPixelsHoriz();
+    int getNumPixelsVert();
+    int getNumPixels();
 
   private:
     Color *rgb;                    						  // rgb points to dynamically allocated array that gives Color object for each (x,y) pair
     int h;                                                // height
-    int w;                                                // width               
+    int w;                                                // width   
+    int numPixels;            
 };
