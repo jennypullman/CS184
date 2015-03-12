2eec64521f30f5eb852e93e97dca2443ffa8a3b7
Lauren Speers <speers.lauren@gmail.com>
2015-03-12 10:07:41 -0700

    Image class working, including writing to png

--- A2/ray_tracer.cpp
+++ A2/ray_tracer.cpp
@@ -16,10 +16,31 @@ inline float sqr(float x) { return x*x; }
 
 void runTests(){
   std::cout << "Running Test 1:  printToFile test" << std::endl;
-  Image testImg = Image(10,10);
-  Color testClr = Color(1.0,0.0,0.0);
-  for (int i = 0; i <100; i++) {
-    testImg.add_Color(i, testClr);
+  Image testImg = Image(12,10);
+  Color testR = Color(1.0,0.0,0.0);
+  Color testY = Color(1.0,1.0,0.0);
+  Color testG = Color(0.0,1.0,0.0);
+  Color testC = Color(0.0,1.0,1.0);
+  Color testB = Color(0.0,0.0,1.0);
+  Color testM = Color(1.0,0.0,1.0);
+  // std::cout << "Color r = "<< testClr.get_r() << std::endl;
+  for (int i = 0; i <20; i++) {
+    testImg.add_Color(i, testR);
+  }
+  for (int i = 20; i <40; i++) {
+    testImg.add_Color(i, testY);
+  }
+  for (int i = 40; i <60; i++) {
+    testImg.add_Color(i, testG);
+  }
+  for (int i = 60; i <80; i++) {
+    testImg.add_Color(i, testC);
+  }
+  for (int i = 80; i <100; i++) {
+    testImg.add_Color(i, testB);
+  }
+  for (int i = 100; i <120; i++) {
+    testImg.add_Color(i, testM);
   }
   char fileName[] = {'t','e','s','t','I','m','g','.','p','n','g', '\0'};
   int code = testImg.printToFile(fileName);
@@ -39,7 +60,6 @@ void runTests(){
 int main(int argc, char *argv[]) {
   runTests();
 
-  std::cout << "Hello World!";
   //handle input (making objects as we go)-->keep list of objects
   //create camera, viewplane, and image object
   //for each pixel on viewplane, make ray between camera and viewplane.
